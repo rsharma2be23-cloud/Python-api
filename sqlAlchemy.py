1b86da316cb1e795a370f436ad2c1e2b6535b760
Rayan <rsharma2_be23@thapar.edu>
2026-07-06 13:19:28 +0530

    update opertion added

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -53,7 +53,7 @@ def create_todo(title:str,db: Session = Depends(get_db)):
         "data":todo
     }
 
-# Reas All Data
+# Read All Data
 @app.get("/todos")
 def get_todos(db:Session = Depends(get_db)):
     todos = db.query(Todo).all()
@@ -63,6 +63,7 @@ def get_todos(db:Session = Depends(get_db)):
         "data": todos
     }
 
+# Read Data bsed on ID
 @app.get("/todos/{todo_id}")
 def get_todo(todo_id= int, db: Session = Depends(get_db)):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
@@ -70,3 +71,21 @@ def get_todo(todo_id= int, db: Session = Depends(get_db)):
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
+
+#Update
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id:int, title:str, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    
+    todo.title = title
+
+    db.commit()
+    db.refresh(todo)
+
+    return{
+        "message":"Todo Updated",
+        "data": todo
+    }
